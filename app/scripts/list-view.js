26bdcd42249bc8ce6a5981630211821bd014598f
Katy Campen <katycampen@gmail.com>
2014-02-19 20:36:06 -0500

    updated main js file

--- app/scripts/list-view.js
+++ app/scripts/list-view.js
@@ -1,11 +1,9 @@
 var ListView = Backbone.View.extend({
+	
 	className: 'list-item',
 
 	createTemplate:_.template($('#list-item-template').text()),
 
-	events: {
-		'click': 'newJumbo'
-	},
 
 	initialize: function(){
 		$('.js-contact-list').prepend(this.el);
@@ -18,12 +16,8 @@ var ListView = Backbone.View.extend({
 		var renderedTemplate = this.createTemplate(this.model.attributes)
 
 		this.$el.html(renderedTemplate)
-	},
-	
-	newJumbo: function(){
-		new MainView({model: this.model})
-		
 	}
+	
 	
 
 });
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -21,3 +21,36 @@ window.contacts = new ContactsCollection()
 	})
 
 });
+
+$('.save-btn-js').on('click', function(){
+
+
+var userProfile = new Contact()
+	if($('.name-js').val()) {userProfile.set({name: $('.name-js').val()})}; 
+	if($('.bio-js').val()) {userProfile.set({bio: $('.bio-js').val()})};
+	if($('.phone-js').val()) {userProfile.set({phone: $('.phone-js').val()})};  
+	if($('.avatar-js').val()) {userProfile.set({avatar: $('.avatar-js').val()})}; 
+	//name: $('.name-js').val(),
+	//bio: $('.bio-js').val(),
+	//phone: $('.phone-js').val(),
+
+
+var newInfo = contacts.add(userProfile);
+
+new ListView({model: userProfile});
+
+userProfile.save();
+
+})
+
+
+// delete. still acts kinda funny
+window.deleteFunction = function(){
+	contacts.each(function(dataStuff){
+		console.log(dataStuff)
+		$.ajax({
+		type: 'delete',
+			url: 'http://0.0.0.0:3000/collections/contacts/'+dataStuff.get("_id")
+		})
+	})
+}
